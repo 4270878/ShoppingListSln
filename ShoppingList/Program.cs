fd456e5015a178b7189f8ef12189fe9011a3bd9a
4270878 <4270878@myuwc.ac.za>
2022-04-28 09:16:13 +0200

    Add project files.

---
+++ ShoppingList/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ShoppingList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
